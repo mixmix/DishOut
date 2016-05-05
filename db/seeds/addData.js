ddfe8a9eb8dd8b2c2c56245e0fc070ebdb847a49
James Sangalli <j.l.sangalli@gmail.com>
2016-05-06 10:27:20 +1200

    add seed and migrate

---
+++ db/seeds/addData.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    //knex('userTable').del(),
+
+    // user table seed
+    knex('userTable').insert({name: 'Bill',email:"clinton@ididnotsleepwiththatwomen.com", hashedPassword:Bxhqhrhabfcbn1312}),
+    knex('userTable').insert({name: 'Ezra', email:"EzraManOfGod@mormon.com", hashedPassword:12xhqhrhabfcb13123412}),
+    knex('userTable').insert({name: 'Ben', email:"ben@scully.com", hashedPassword:341Bxhqhrhabfcbn1312})
+
+
+    // Deletes ALL existing entries
+    //knex('hostTable').del(),
+
+    // host table seed
+    knex('hostTable').insert({eventId: 1, userId: 3}),
+    knex('hostTable').insert({eventId: 2, userId: 2}),
+    knex('hostTable').insert({eventId: 3, userId: 1})
+
+    //guest table seed
+    knex('guestTable').insert({eventId: 1, userId: 3}),
+    knex('guestTable').insert({eventId: 2, userId: 2}),
+    knex('guestTable').insert({eventId: 3, userId: 1})
+
+    //event table seed
+    knex('eventsTable').insert({name: 1, date: "21/10",time: "10:00" ,description: "party at my house", }),
+    knex('eventsTable').insert({name: 2, date: "30/11",time: "23:00",description: "party at my house", }),
+    knex('eventsTable').insert({name: 3, date: "20/12",time: "20:00" ,description: "party at my house", })
+  );
+  );
+};
