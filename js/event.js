af45219ad745e032f1447205b40c6de848f011be
Ezra Fermanis <ezra.fermanis@gmail.com>
2016-05-05 16:15:42 +1200

    event details

--- js/event.js
+++ js/event.js
@@ -1,9 +1,15 @@
 //Details about the event
-var event = {
-  name: "James Potluck Dinner",
-  date: "12 May",
-  time: "8pm",
-  description: "All you can eat buffet. There will be food, booze and girls. The theme is the 80s"
-};
 
+<div id='event'>
+<h1>James Potluck Dinner</h1>
+<h4>Date: 12 May</h4>
+<h4>Time: 8pm</h4>
+<h4>description: All you can eat buffet. There will be food, booze and girls. The theme is the 80s</h4>
+</div>
+// var event = {
+//   name: "James Potluck Dinner",
+//   date: "12 May",
+//   time: "8pm",
+//   description: "All you can eat buffet. There will be food, booze and girls. The theme is the 80s"
+// };
 modules.export = event;
