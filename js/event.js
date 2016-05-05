83908ec39403cd537583b1170515341e6e58f066
Ezra Fermanis <ezra.fermanis@gmail.com>
2016-05-05 15:48:31 +1200

    Added event details object

---
+++ js/event.js
@@ -0,0 +1,9 @@
+//Details about the event
+var event = {
+  name: "James Potluck Dinner",
+  date: "12 May",
+  time: "8pm",
+  description: "All you can eat buffet. There will be food, booze and girls. The theme is the 80s"
+};
+
+modules.export = event;
