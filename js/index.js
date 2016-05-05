ccf4541f6988edc424326aa8bf38b7cde6ed24f3
Ezra Fermanis <ezra.fermanis@gmail.com>
2016-05-06 02:06:09 +1200

    added select dishes

--- js/index.js
+++ js/index.js
@@ -3,10 +3,13 @@ $(function(){
   var addDishFields = require("./addDishFields")
   var guestDishList = require("./guestDishList")
   var event = require("./event")
+  var selectDishes = require("./selectDishes")
 
+  $("#selectDishes").html(selectDishes)
   $("#addDishFields").html(addDishFields)
   $("#dishList").html(guestDishList)
   $("#event").html(event)
+
   require("./addDish")
 
 });
---
+++ js/selectDishes.js
@@ -0,0 +1,25 @@
+
+var selectDishes = [
+  '<div>',
+  '<h3>Enter the types of dishes you need for the event (e.g starters, main, snack, dessert etc.)</h2>',
+  '<input id="selectDishes" type="text">',
+  '</div>'
+];
+
+module.exports = selectDishes;
+
+
+// 
+// <script>
+// var allInputs = $( ":input" );
+// var formChildren = $( "form > *" );
+// $( "#messages" ).text( "Found " + allInputs.length + " inputs and the form has " +
+//   formChildren.length + " children." );
+//
+// $( "form" ).submit(function( event ) {
+//   event.preventDefault();
+// });
+// </script>
+//
+// </body>
+// </html>
