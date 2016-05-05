a6a0cd5d66a18808cf17c34aa47c628170bfb2c8
James Sangalli <j.l.sangalli@gmail.com>
2016-05-05 16:25:22 +1200

    added event

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ $(function(){
 
   var addDishFields = require("./addDishFields")
   var guestDishList = require("./guestDishList")
+  var event = require("./event")
 
   $("#addDishFields").html(addDishFields)
   $("#dishList").html(guestDishList)
+  $("#event").html(event)
   require("./addDish")
-  
+
 });
