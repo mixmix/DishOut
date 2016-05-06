3e0dfd4bb07d0f447d2abea8b3c85efa9d750d5f
xxPicasso <ben@moregolf.co.nz>
2016-05-06 15:09:08 +1200

    deleted all html files because now using jade

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
-$(function(){
-  var addDishFields = require("./addDishFields")
-  var guestDishList = require("./guestDishList")
-  var event = require("./event")
-  var createEventView = require("./createEventView")
+$(init)
 
-  $("#addDishFields").html(addDishFields)
-  $("#dishList").html(guestDishList)
-  $("#event").html(event)
-  require("./addDish")
-  $("#createEvent").html(createEventView)
-});
+function init () {
+  
+}
