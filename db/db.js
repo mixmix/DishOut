891663b8e6f98df1e5d582fc214bb95c356a7a36
James Sangalli <j.l.sangalli@gmail.com>
2016-05-07 11:58:25 +1200

    add sessions and hashing

--- db/db.js
+++ db/db.js
@@ -4,17 +4,40 @@ var bcrypt = require('bcrypt')
 const saltRounds = 10;
 
 function createUser(name,email,password,cb){
-  knex("users").insert({
-    name:name,
-    email:email,
-    hashedPassword:password
+  bcrypt.hash(password,saltRounds,function(err,hash){
+    knex("users").insert({
+      name:name,
+      email:email,
+      hashedPassword:hash
+    })
+    .then(function(data){
+      cb(null,data[0])
+    })
+    .catch(function(err){
+      cb(err)
+    })
   })
-  .then(function(data){
-    cb(null,data)
-  })
-  .catch(function(err){
-    cb(err)
-  })
+
+}
+
+function login(email, password, cb){
+  knex.select().where("email", email).table("users")
+    .then(function(data){
+      console.log("here is the user's hashed password: ",data[0].hashedPassword)
+      bcrypt.compare(password , data[0].hashedPassword, function(err,response){
+        if(response){
+          console.log("successful login", response)
+          cb(null, data[0])
+        }
+        else{
+          console.log("failed login")
+          cb(err)
+        }
+      })
+    })
+    .catch(function(err){
+      cb(err)
+    })
 }
 
 function getEventsAttending(id,cb){
@@ -103,15 +126,7 @@ function getTenativeEvents(id, cb){
     })
 }
 
-function login(email, password, cb){
-  knex.select().where("email", email).table("users")
-    .then(function(data){
-      cb(null, data[0])
-    })
-    .catch(function(err){
-      cb(err)
-    })
-}
+
 
 module.exports = {
   createUser: createUser,
