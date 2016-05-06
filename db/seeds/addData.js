f85440c58a44a5f7fb1280dc78f5f1a27004f03c
James Sangalli <j.l.sangalli@gmail.com>
2016-05-06 15:02:20 +1200

    db working

--- db/seeds/addData.js
+++ db/seeds/addData.js
@@ -1,35 +1,43 @@
 exports.seed = function(knex, Promise) {
   return Promise.join(
     // Deletes ALL existing entries
-    knex('userTable').del(),
+    knex('users').del(),
 
-    // user table seed
-    knex('userTable').insert({name: 'Bill',email:"clinton@ididnotsleepwiththatwomen.com", hashedPassword:"Bxhqhrhabfcbn1312"}),
-    knex('userTable').insert({name: 'Ezra', email:"EzraManOfGod@mormon.com", hashedPassword:"12xhqhrhabfcb13123412"}),
-    knex('userTable').insert({name: 'Ben', email:"ben@scully.com", hashedPassword:"341Bxhqhrhabfcbn1312"}),
-
+    // user s seed
+    knex('users').insert({name: 'Bill',email:"clinton@ididnotsleepwiththatwomen.com", hashedPassword:"Bxhqhrhabfcbn1312"}),
+    knex('users').insert({name: 'Ezra', email:"EzraManOfGod@mormon.com", hashedPassword:"12xhqhrhabfcb13123412"}),
+    knex('users').insert({name: 'Ben', email:"ben@scully.com", hashedPassword:"341Bxhqhrhabfcbn1312"}),
 
     // Deletes ALL existing entries
-    knex('hostTable').del(),
+    knex('events').del(),
+    //event s seed
+    knex('events').insert({name: "Bill Clinton", date: "21/10",time: "10:00" , description: "party at my house", location:"The white house" }),
+    knex('events').insert({name: "Jack John", date: "30/11",time: "23:00", description: "party at my house", location: "12 orange rd"}),
+    knex('events').insert({name: "Chairman Liu", date: "20/12",time: "20:00" , description: "party at my house", location:"tiananmen square"}),
 
-    // host table seed
-    knex('hostTable').insert({eventId: 1, userId: 3}),
-    knex('hostTable').insert({eventId: 2, userId: 2}),
-    knex('hostTable').insert({eventId: 3, userId: 1}),
+    // Deletes ALL existing entries
+    knex('hosts').del(),
+
+    // host s seed
+    knex('hosts').insert({eventId: 1, userId: 3}),
+    knex('hosts').insert({eventId: 2, userId: 2}),
+    knex('hosts').insert({eventId: 3, userId: 1}),
 
      // Deletes ALL existing entries
-    knex('guestTable').del(),
+    knex('guests').del(),
 
-    //guest table seed
-    knex('guestTable').insert({eventId: 1, userId: 3}),
-    knex('guestTable').insert({eventId: 2, userId: 2}),
-    knex('guestTable').insert({eventId: 3, userId: 1}),
+    //guest s seed
+    knex('guests').insert({eventId: 1, userId: 3}),
+    knex('guests').insert({eventId: 2, userId: 2}),
+    knex('guests').insert({eventId: 3, userId: 1}),
 
     // Deletes ALL existing entries
-    knex('eventsTable').del(),
-    //event table seed
-    knex('eventsTable').insert({name: "Bill Clinton", date: "21/10",time: "10:00" , description: "party at my house", location:"The white house" }),
-    knex('eventsTable').insert({name: "Jack John", date: "30/11",time: "23:00", description: "party at my house", location: "12 orange rd"}),
-    knex('eventsTable').insert({name: "Chairman Liu", date: "20/12",time: "20:00" , description: "party at my house", location:"tiananmen square"})
+   knex('dishes').del(),
+
+   //guest s seed
+   knex('dishes').insert({course: "Main", name: "Butter chicken", eventId:1,userId:3}),
+   knex('dishes').insert({course: "Main", name: "Butter chicken", eventId:2,userId:2}),
+   knex('dishes').insert({course: "Main", name: "Butter chicken", eventId:3,userId:1})
+
   );
 };
