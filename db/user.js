2c530aeee174355739b27b234ecc70d24c7d236f
James Sangalli <j.l.sangalli@gmail.com>
2016-05-07 18:27:47 +1200

    fixed knex config, created tests

---
+++ db/user.js
@@ -0,0 +1,62 @@
+var knexConfig = require('../knexfile')
+var knex = require('knex')(knexConfig[process.env.NODE_ENV || "development"])
+var bcrypt = require('bcrypt')
+const saltRounds = 10
+
+function createUser(userObj, cb){
+  bcrypt.hash(userObj.password,saltRounds,function(err,hash){
+    var newUserObj = {
+      name: userObj.name,
+      email: userObj.email,
+      hashedPassword: hash
+    }
+    if (typeof userObj.name !== 'string') {
+      cb("invalid user name")
+      return
+    }
+    knex("users").insert(newUserObj)
+      .then(function(data){
+        cb(null,data[0])
+      })
+      .catch(function(err){
+        cb(err)
+      })
+  })
+
+}
+
+function login(email, password, cb){
+  knex.select().where("email", email).table("users")
+    .then(function(data){
+      console.log("here is the user's hashed password: ",data[0].hashedPassword)
+      bcrypt.compare(password , data[0].hashedPassword, function(err,response){
+        if(response){
+          console.log("successful login", response)
+          cb(null, data[0])
+        }
+        else{
+          console.log("failed login")
+          cb(err)
+        }
+      })
+    })
+    .catch(function(err){
+      cb(err)
+    })
+}
+
+function getUserByEmail(email){
+  knex.select().where("email",email).table("users")
+    .then(function(data){
+      console.log("Here is the users data: ", data)
+    })
+    .catch(function(err){
+      if (err) throw err
+    })
+}
+
+module.exports = {
+  createUser:createUser,
+  login:login,
+  getUserByEmail:getUserByEmail
+}
--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,64 @@
-var test = require("tape")
+var redtape = require('redtape')
+var db = require("../db/db")
+var knexConfig = require('../knexfile')
+var knex = require('knex')(knexConfig["development"])
+var user = require("../db/user")
+
+var createUserTestObj = {
+  name:"Jill",
+  email:"Jill@hill.com",
+  password:"123"
+}
+
+var createUserUni = {
+  name:"Ben",
+  email:"ben@scully.com",
+  password:"44"
+}
+
+var test = redtape({
+  beforeEach: function (callback) {
+    return knex.migrate.latest()
+      .then(function () {
+        return knex.seed.run()
+      })
+      .then(function () {
+        callback()
+      })
+  },
+
+  afterEach: function (callback) {
+    knex.migrate.rollback()
+      .then(function () {
+        callback()
+      })
+  }
+})
+
+test('setup', function (t) {
+  knex.migrate.rollback()
+    .then(function () {
+      t.end()
+    })
+})
+
+test("tests create user db function",function(t){
+  user.createUser(createUserTestObj,function(err,data){
+    if (err) {
+      console.log(err)
+    }
+    t.ok(data,"Something came back!")
+    t.equal(typeof data, 'number', "it returns an id number")
+    t.end()
+  })
+})
+
+test("tests create user db function",function(t){
+  user.createUser(createUserUni,function(err,data){
+    if (err) {
+      console.log(err)
+    }
+    t.true(err)
+    t.end()
+  })
+})
