9b3c3a3d3864b55f5eeace82c7d6c1955493db3b
James Sangalli <j.l.sangalli@gmail.com>
2016-05-09 11:08:22 +1200

    refactor routes, passport

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+// app/routes.js
+var basic_routes = require('./basic_routes')
+var user_routes = require('./user_routes')
+var event_routes = require('./event_routes')
+var dish_routes = require('./dish_routes')
+var guest_routes = require('./guest_routes')
+
+
+
+module.exports = function(app, passport) {
+  app.use('/', basic_routes)
+  app.use('/user', user_routes)
+  app.use('/event', event_routes)
+  app.use('/dish', dish_routes)
+  app.use('/guest', guest_routes)
+}
--- server.js
+++ server.js
@@ -5,19 +5,17 @@ var port = process.env.PORT || 8080
 var passport = require('passport')
 var Strategy = require('passport-facebook').Strategy;
 
-var basic_routes = require('./routes/basic_routes')
-var user_routes = require('./routes/user_routes')
-var event_routes = require('./routes/event_routes')
-var dish_routes = require('./routes/dish_routes')
-var guest_routes = require('./routes/guest_routes')
-
 var session = require('express-session')
 
+// required for passport
 app.use(session({
   secret: 'ssshhhhhh! Top secret!',
   saveUninitialized: true,
   resave: true
 }))
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+// app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -25,12 +23,7 @@ app.set('view engine', 'pug')
 app.set('views', __dirname + '/public/views')
 app.use(express.static(__dirname + '/public'))
 
-app.use('/', basic_routes)
-app.use('/user', user_routes)
-app.use('/event', event_routes)
-app.use('/dish', dish_routes)
-app.use('/guest', guest_routes)
-
+require("./routes/index.js")(app,passport)
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port + '\n')
